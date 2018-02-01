8f76e32826cd82e273abad81514674862b2b3a6a
Bas van Dijk <bas@usto.nl>
2018-02-01 20:05:12 +0100

    Implemented output pin handling

--- gpio-test.js
+++ gpio-test.js
@@ -1,20 +1,32 @@
 var Gpio = require('pigpio').Gpio;
 
-let button = new Gpio(18, {
-    mode: Gpio.INPUT,
-    pullUpDown: Gpio.PUD_UP,
-    edge: Gpio.EITHER_EDGE
-});
 // let button = new Gpio(18, {
 //     mode: Gpio.INPUT,
 //     pullUpDown: Gpio.PUD_UP,
 //     edge: Gpio.EITHER_EDGE
 // });
 
-// led = new Gpio(17, { mode: Gpio.OUTPUT });
+led = new Gpio(24, { mode: Gpio.OUTPUT });
 
-console.log('read', button.digitalRead());
+dutyCycle = 0;
 
-button.on('interrupt', function (level) {
-    console.log(level);
-});
+// while(true) { 
+//     led.pwmWrite(255);
+// }
+
+led.digitalWrite(1);
+
+// setInterval(function () {
+//   led.pwmWrite(dutyCycle);
+
+//   dutyCycle += 5;
+//   if (dutyCycle > 255) {
+//     dutyCycle = 0;
+//   }
+// }, 20);
+
+// console.log('read', button.digitalRead());
+
+// button.on('interrupt', function (level) {
+//     console.log(level);
+// });
--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ import DomustoPlugin from '../../domusto/DomustoPlugin';
 // INTERFACES
 import { Domusto } from '../../domusto/DomustoTypes';
 import DomustoSignalHub from '../../domusto/DomustoSignalHub';
+import DomustoDevicesManager from '../../domusto/DomustoDevicesManager';
 
 // PLUGIN SPECIFIC
 // import * as Gpio from 'pigpio';
@@ -22,9 +23,11 @@ let Gpio = require('pigpio').Gpio;
  */
 class DomustoGPIO extends DomustoPlugin {
 
-    GPIO_HIGH = 1;
-    GPIO_LOW = 0;
-    button;
+    private inputPinsInstances = [];
+    private outputPinsInstances = [];
+
+    private GPIO_HIGH = 1;
+    private GPIO_LOW = 0;
 
     /**
      * Creates an instance of DomustoGPIO.
@@ -43,61 +46,122 @@ class DomustoGPIO extends DomustoPlugin {
 
         this.pluginConfiguration = pluginConfiguration;
 
+        this.console.header('Initialising GPIO pins');
+
         for (let pin of this.pluginConfiguration.settings.pins) {
 
             const pinDirection = pin.type.split('/')[0];
             const pinType = pin.type.split('/')[1];
 
-            this.console.debug('GPIO', pin.pinNumber, pin.type);
+            this.console.debug('Pin', pin.pinNumber, pin.type);
 
-            this.button = new Gpio(pin.pinNumber, {
-                mode: pinDirection === 'input' ? Gpio.INPUT : Gpio.OUTPUT,
-                pullUpDown: pin.resistor === 'pullUp' ? Gpio.PUD_UP : Gpio.PUD_DOWN,
-                edge: Gpio.EITHER_EDGE
-            });
+            if (pinDirection === 'input') {
 
-            this.button.on('interrupt', pinState => {
-
-                this.console.debug('GPIO 18 (INT)', pinState);
-
-                console.log(pinType);
-
-                if (pinType === 'NO') {
-                    pinState = pinState === 1 ? 0 : 1;
-                }
-
-                this.broadcastSignal('GPIO18', {
-                    state: pinState === 1 ? 'on' : 'off'
+                let input = new Gpio(pin.pinNumber, {
+                    mode: pinDirection === 'input' ? Gpio.INPUT : Gpio.OUTPUT,
+                    pullUpDown: pin.resistor === 'pullUp' ? Gpio.PUD_UP : Gpio.PUD_DOWN,
+                    edge: Gpio.EITHER_EDGE
                 });
 
-            });
+                input.on('interrupt', pinState => {
 
+                    // this.broadcastPinState(pin.pinNumber, pinState, pinType, 'interrupt');
 
+                    let deviceId = 'GPIO' + pin.pinNumber;
+
+                    const devices = DomustoDevicesManager.getDevicesByDeviceId(deviceId);
+
+                    for (let device of devices) {
+
+                        let devicePinState = pinState;
+
+                        if (pinType === 'NO') {
+                            devicePinState = (pinState === 1 ? 0 : 1);
+                        }
+
+                        // Broadcast a signal as if it was send from the client
+                        this.broadcastSignal(device.plugin.deviceId, {
+                            state: devicePinState === 1 ? 'on' : 'off'
+                        }, Domusto.SignalSender.client);
+
+                    }
+
+                });
+
+                this.inputPinsInstances.push({
+                    pinData: pin,
+                    instance: input
+                });
+
+            }
+
+            if (pinDirection === 'output') {
+
+                let output = new Gpio(pin.pinNumber, {
+                    mode: Gpio.OUTPUT,
+                });
+
+                this.outputPinsInstances.push({
+                    pinData: pin,
+                    instance: output
+                });
+
+            }
         }
 
-        setTimeout(() => this.refreshPinStatus(), 1000);
+        // We need to wait for the IO ports to be ready
+        setTimeout(() => this.refreshPinsStatus(), 1000);
 
     }
 
-    refreshPinStatus() {
 
-        let pinState = this.button.digitalRead();
+    onSignalReceivedForPlugin(signal: Domusto.Signal) {
 
-        this.console.debug('GPIO 18 (READ)', pinState);
+        for (let outputPin of this.outputPinsInstances) {
 
-        // if (pinType === 'NO') {
+            let pinName = 'GPIO' + outputPin.pinData.pinNumber;
+
+            if (pinName === signal.deviceId) {
+
+                outputPin.instance.digitalWrite(signal.data['state'] === 'on' ? 1 : 0);
+
+                this.broadcastSignal(pinName, {
+                    state: signal.data['state'],
+                });
+
+                break;
+            }
+
+        }
+
+    }
+
+    refreshPinsStatus() {
+
+        for (let inputPin of this.inputPinsInstances) {
+
+            let pinType = inputPin.pinData.type.split('/')[1];
+            let pinState = inputPin.instance.digitalRead();
+
+            this.broadcastPinState(inputPin.pinData.pinNumber, pinState, pinType, 'read');
+        }
+
+    }
+
+    broadcastPinState(pinNumber, pinState, pinType, readEventType) {
+
+        this.console.debug(`GPIO ${pinNumber} (${pinType}) -> ${readEventType}:`, pinState);
+
+        if (pinType === 'NO') {
             pinState = pinState === 1 ? 0 : 1;
-        // }
+        }
 
-        console.log('STATE', pinState);
-
-        this.broadcastSignal('GPIO18', {
+        this.broadcastSignal(`GPIO${pinNumber}`, {
             state: pinState === 1 ? 'on' : 'off'
         });
 
     }
 
-    onSignalReceivedForPlugin(signal: Domusto.Signal) { }
 
 }
 
